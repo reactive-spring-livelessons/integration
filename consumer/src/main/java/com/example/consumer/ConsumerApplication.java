6308ee4bb7328fd6633bf51105f4b9e9126bb292
Josh Long <starbuxman@gmail.com>
2018-04-05 16:26:21 -0700

    consumer and producer

--- consumer/src/main/java/com/example/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/example/consumer/ConsumerApplication.java
@@ -1,62 +1,25 @@
 package com.example.consumer;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.context.annotation.Bean;
-import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.dsl.IntegrationFlows;
-import org.springframework.integration.handler.GenericHandler;
-import org.springframework.messaging.support.MessageBuilder;
 import reactor.core.publisher.Flux;
 
-import java.time.Duration;
-import java.time.Instant;
-
-@Log
+@EnableBinding(Sink.class)
 @SpringBootApplication
 public class ConsumerApplication {
 
-		@EnableBinding(Sink.class)
-		public static class StreamListenerSink {
-
-				@StreamListener
-				public void process(@Input(Sink.INPUT) Flux<String> messages) {
-						messages.subscribe(g -> log.info("new greeting: " + g));
-				}
-		}
-
-		@Bean
-		IntegrationFlow flow() {
-
-				Flux<Greeting> delayElements = Flux
-					.<Greeting>generate(sink -> sink.next(new Greeting("Hello, world @" + Instant.now().toString() + "!")))
-					.delayElements(Duration.ofSeconds(1));
-
-				return IntegrationFlows.from(delayElements
-					.map(g -> MessageBuilder.withPayload(g).build()))
-					.handle((GenericHandler<Greeting>) (payload, headers) -> {
-							log.info("new Publisher<T>-powered message: " + payload.getMessage());
-							return null;
-					})
-					.get();
+		@StreamListener
+		public void process(@Input(Sink.INPUT) Flux<String> incomingStrings) {
+				incomingStrings
+					.map(String::toUpperCase)
+					.subscribe(System.out::println);
 		}
 
 		public static void main(String[] args) {
 				SpringApplication.run(ConsumerApplication.class, args);
 		}
-}
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-class Greeting {
-		private String message;
 }
--- producer/src/main/java/com/example/producer/ProducerApplication.java
+++ producer/src/main/java/com/example/producer/ProducerApplication.java
@@ -1,45 +1,36 @@
 package com.example.producer;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
 
-import java.time.Instant;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
-@EnableBinding(ProducerChannels.class)
 @SpringBootApplication
+@EnableBinding(Source.class)
 public class ProducerApplication {
 
+		private final Log log = LogFactory.getLog(getClass());
+
 		@Bean
-		ApplicationRunner producer(ProducerChannels channels) {
+		ApplicationRunner producer(Source source) {
 				return args -> {
-						MessageChannel output = channels.greetings();
-						ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
-						service
-							.scheduleWithFixedDelay(() -> {
-									Message<String> message = MessageBuilder.withPayload("hello, @ " + Instant.now().toString()).build();
-									output.send(message);
-							}, 1, 1, TimeUnit.SECONDS);
+						int i = 0;
+						while (true) {
+								Message<String> message = MessageBuilder.withPayload("Hello, #" + i++).build();
+								Thread.sleep(1000);
+								this.log.info("sending " + message.getPayload() + ".");
+								source.output().send(message);
+						}
 				};
 		}
 
 		public static void main(String[] args) {
 				SpringApplication.run(ProducerApplication.class, args);
 		}
-}
-
-
-interface ProducerChannels {
-
-		@Output
-		MessageChannel greetings();
 }
